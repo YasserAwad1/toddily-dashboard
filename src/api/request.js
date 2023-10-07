3e8cb1a19d28a42dd01b9af4e24f153e4c066ff6
YasserAwad1 <Yasser.awad1@hotmail.com>
2023-10-07 16:49:28 -0400

    7/10

--- src/api/request.js
+++ src/api/request.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
-const client = axios.create({ baseURL: 'http://207.154.207.148/api' })
+const client = axios.create({ baseURL: 'https://api.toddily-pre.com/api' })
 export const request = async ({ ...options }) => {
     client.defaults.headers.common.Authorization = `Bearer ${Cookies.get('_toddily_admin_token')}`
     return client(options)
         .then((res) => res)
 }
 
-export const baseURLImage = 'http://207.154.207.148'
+export const baseURLImage = 'https://api.toddily-pre.com'
